371a52d6bbf5a0c20aaf8c72f751a0ea75a064e6
LagneauJonathan <lagneaujonathan@gmail.com>
2020-01-09 13:46:24 +0100

    Update CPatient.java
    Ajout des méthodes et modification des nom et type de variable pour plus de cohérence

--- src/be/BiscontiLagneau/POJO/CPatient.java
+++ src/be/BiscontiLagneau/POJO/CPatient.java
@@ -1,23 +1,55 @@
 package be.BiscontiLagneau.POJO;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.*;
+
+import be.BiscontiLagneau.DAO.DAOConnexion;
+import be.BiscontiLagneau.DAO.DAOPatient;
+import be.BiscontiLagneau.Enum.Genre;
 
 public class CPatient extends CPersonne implements Serializable{
-	//Attributs
+	
 	private static final long serialVersionUID = 1L;
-	int nss; //N° sécurité sociale
+	//Attributs
+	int ID_Patient;
+	long nrn; //N° de registre national
 	
 	//Constructeurs
 	public CPatient() {}
 	
-	//Accesseurs
-	public int getNss()
-	{
-		return nss;
+	public CPatient(String nom, String prenom, LocalDate dateNaissance, String telephone, Genre sexe, String adresse, int ID_Patient, long nrn) {
+		super(nom, prenom, dateNaissance, telephone, sexe, adresse);
+		this.ID_Patient = ID_Patient;
+		this.nrn = nrn;
+	}
+
+	//Methodes
+	public ArrayList<CPatient> recuperTousPatients(){
+		DAOPatient daoPatient = new DAOPatient(DAOConnexion.getInstance());
+		return daoPatient.recupererTout();
 	}
 	
-	public void setNss(int nss)
+	public Boolean ajouter(CPatient patient) {
+		DAOPatient daoPatient = new DAOPatient(DAOConnexion.getInstance());
+		return daoPatient.ajouter(patient);
+	}
+	//Accesseurs
+	public int getID_Patient() {
+		return ID_Patient;
+	}
+
+	public void setID_Patient(int iD_Patient) {
+		ID_Patient = iD_Patient;
+	}
+	
+	public long getNrn()
 	{
-		this.nss = nss;
+		return nrn;
+	}
+	
+	public void setNrn(long nrn)
+	{
+		this.nrn = nrn;
 	}
 }
