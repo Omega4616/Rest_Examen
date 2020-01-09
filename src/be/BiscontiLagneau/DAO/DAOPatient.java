11c40dd57454c50480c865853aa62a9a9dbc9e00
LagneauJonathan <lagneaujonathan@gmail.com>
2020-01-09 13:47:54 +0100

    Update DAOPatient.java
    Ajout de la méthode pour ajouter un patient,.
    Suppression de l'abstract qui ne doit pas s'y trouver sur les classes filles de ce projet.

--- src/be/BiscontiLagneau/DAO/DAOPatient.java
+++ src/be/BiscontiLagneau/DAO/DAOPatient.java
@@ -1,46 +1,115 @@
 package be.BiscontiLagneau.DAO;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import be.BiscontiLagneau.Enum.Genre;
+import be.BiscontiLagneau.POJO.CMedicament;
 import be.BiscontiLagneau.POJO.CPatient;
+import oracle.jdbc.OracleTypes;
 
-public abstract class DAOPatient extends DAO<CPatient>{
+public class DAOPatient extends DAO<CPatient>{
 
 	public DAOPatient(Connection conn) {
 		super(conn);
 		// TODO Auto-generated constructor stub
 	}
 
-	public CPatient getPatient(int nss)
-	{
-		CPatient patient = null;
+	@Override
+	public boolean ajouter(CPatient patient) {
+		Boolean resultAjout = false;
+		 try
+	        {
+	        	//
+	        	// AjouterPatient (var_nss NUMBER, var_nom VARCHAR2, var_prenom VARCHAR2, var_adresse VARCHAR2,
+			 	// var_naissance DATE, var_sexe VARCHAR2,var_tel VARCHAR2, err_code OUT NUMBER)
+	        	//
+	        	String callStoreProc = "{call AjouterPatient(?,?,?,?,?,?,?,?)}";
+	        	CallableStatement callableStatement = this.connect.prepareCall(callStoreProc);
+	        	callableStatement.setLong(1,patient.getNrn());
+	        	callableStatement.setString(2, patient.getNom());
+	        	callableStatement.setString(3, patient.getPrenom());
+	        	callableStatement.setString(4, patient.getAdresse());
+	        	callableStatement.setDate(5, java.sql.Date.valueOf(patient.getDateNaissance()));
+	        	callableStatement.setString(6, patient.getSexe().toString());
+	        	callableStatement.setString(7, patient.getTelephone());
+	        	callableStatement.registerOutParameter(8, java.sql.Types.INTEGER);	
+	        	callableStatement.execute();
+	        	
+	        	Integer codeResponse = (Integer) callableStatement.getObject(8);
+	        	if(codeResponse != null){
+	        		
+	        		if (codeResponse == -1) {
+						resultAjout = true;
+					}
+	        		if (codeResponse == 0) {
+						resultAjout = false;
+					}
+	        	}
+	        }
+	        catch(SQLException e)
+	        {
+	            e.printStackTrace();
+	            resultAjout = false;
+	        }
+	        return resultAjout;
+	}
+
+	@Override
+	public boolean supprimer(CPatient obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifier(CPatient obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CPatient chercher(CPatient obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<CPatient> recupererTout() {
+		ArrayList<CPatient> ListePatients = new ArrayList<CPatient>();
         try
         {
-            ResultSet result = this.connect.createStatement(
-                    ResultSet.TYPE_SCROLL_INSENSITIVE,
-                    ResultSet.CONCUR_READ_ONLY	).executeQuery("SELECT * FROM Patients PA "
-                    		+ "INNER JOIN Personnes PE ON PA.id_personne = PE.id_personne "
-                    		+ "WHERE nss = " + nss);
+        	//
+        	// recupTousPatients(c_patients OUT SYS_REFCURSOR)
+        	//
+        	String callStoreProc = "{call recupTousPatients(?)}";
+        	CallableStatement callableStatement = this.connect.prepareCall(callStoreProc);
+        	callableStatement.registerOutParameter(1, OracleTypes.CURSOR);	
+        	callableStatement.execute();
+        	
+        	ResultSet rs = (ResultSet) callableStatement.getObject(1);
+        	while (rs.next()) {
+				CPatient cPatient = new CPatient();
+				cPatient.setID_Patient(rs.getInt("ID_Personne"));
+				cPatient.setNom(rs.getString("nom"));
+				cPatient.setPrenom(rs.getString("prenom"));
+				cPatient.setDateNaissance(rs.getDate("naissance").toLocalDate());
+				cPatient.setAdresse(rs.getString("adresse"));
+				cPatient.setSexe(Genre.fromString(rs.getString("sexe").toLowerCase()));
+				cPatient.setTelephone(rs.getString("telephone"));
+				cPatient.setNrn(rs.getLong("nss"));
+				ListePatients.add(cPatient);
+			}
 
-            while(result.next())
-            {
-                patient = new CPatient();
-                patient.setNom(result.getString("nom"));
-                patient.setPrenom(result.getString("prenom"));
-                patient.setAdresse(result.getString("adresse"));
-                //patient.setDateNaissance(result.getDate("naissance"));
-                patient.setNss(result.getInt("nss"));
-                patient.setTelephone(result.getString("telephone"));
-                //sexe
-            }
         }
         catch(SQLException e)
         {
             e.printStackTrace();
         }
-        return patient;
+        return ListePatients;
 	}
+
 }
